56929cfe08ec74b4c59fc48fa2c42b1b081f4305
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-09-20 17:06:58 +0200

    Add project files.

---
+++ frmMain.cs
@@ -0,0 +1,10 @@
+namespace AS2223_4G_INF_CangiottiFederico_RubricaCSV
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
